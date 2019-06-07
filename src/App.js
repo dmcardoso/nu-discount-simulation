a0754e5ead456eef785bb779e1baf72a0cfe19d4
dmcardoso <qualquercoisa6568@hotmail.com>
2019-06-07 12:47:05 -0300

    refatoração do scss

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import './App.scss';
+import Input from './components/Input';
 
 function App() {
     return (
         <div className="App">
+            <Input/>
         </div>
     );
 }
---
+++ src/components/Input/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.scss';
+
+const Input = (props) => (
+    <input type="text" className="Input fs-20" {...props}/>
+);
+
+export default Input;
